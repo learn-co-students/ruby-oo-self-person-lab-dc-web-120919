1b9f042d04ff97862894615a997591305abb9664
Sean Beach <sean.beach1@yahoo.com>
2019-12-11 15:01:15 -0500

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -47,5 +47,44 @@ class Person
         end
     end
     
+    def get_paid(salary)
+       @bank_account = salary + @bank_account
+        return "all about the benjamins"
+    end
+
+    def take_bath
+      self.hygiene = self.hygiene + 4
+      return '♪ Rub-a-dub just relaxing in the tub ♫'
+    end
+
+    def work_out
+        self.happiness = self.happiness + 2
+        self.hygiene = self.hygiene - 3
+        return '♪ another one bites the dust ♫'
+    end
+
+    def call_friend(callee)
+        self.happiness = self.happiness + 3
+        callee.happiness = callee.happiness + 3
+        return "Hi #{callee.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        if topic == 'politics'
+            self.happiness = self.happiness - 2
+            person.happiness = person.happiness - 2
+            return 'blah blah partisan blah lobbyist'
+        elsif topic == 'weather'
+            self.happiness = self.happiness + 1
+            person.happiness = person.happiness + 1
+            return 'blah blah sun blah rain'
+        else topic == 'programming'
+            return 'blah blah blah blah blah'
+        end
+    end
+
+    
+    
+
     
 end# your code goes here
