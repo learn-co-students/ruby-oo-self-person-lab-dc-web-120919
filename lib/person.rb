18f8d6fff1c56b66f3a767366b591bedb426709d
github username <github email address>
2019-12-11 12:07:40 -0500

    set up attr

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,17 @@
+require 'pry'
+
 class Person
     
-    def initialize(name, bank_account=25, happiness_points=8, hygiene_points=8)
+    attr_reader :name
+    attr_writer 
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name, bank_account=25, happiness=8, hygiene=8)
         @name = name
         @bank_account = bank_account
-        @happiness_points = happiness_points
+        @happiness = happiness
+        @hygiene = hygiene
+    end
     
     
 end# your code goes here
