9053f182fdc160337d74cfe698c04d2f03bfdaa2
github username <github email address>
2019-12-11 11:57:51 -0500

    set up class and initialize method

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,9 @@
-# your code goes here
+class Person
+    
+    def initialize(name, bank_account=25, happiness_points=8, hygiene_points=8)
+        @name = name
+        @bank_account = bank_account
+        @happiness_points = happiness_points
+    
+    
+end# your code goes here
